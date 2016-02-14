1180d59e760c0ab3ac6c8a51a29b3055a29fa951
Joanna <joannakolbe@gmail.com>
2016-02-13 23:11:41 -0500

    kolbipol gallery, contact form

--- js/kolbipol.js
+++ js/kolbipol.js
@@ -4,4 +4,33 @@ $(document).ready(function(){
 		horizontalOffset: 50,
   		verticalOffset: 100
 	});
+
+
+	$('.image-gallery').magnificPopup({
+        delegate: 'a',
+        type: 'image',
+        tLoading: 'Loading image #%curr%...',
+        mainClass: 'mfp-img-mobile',
+        gallery: {
+            enabled: true,
+            navigateByImgClick: true,
+            preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+        },
+        image: {
+            tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+            titleSrc: function(item) {
+            return '';
+            }
+        }
+    });
+
+    $("#mobile-gallery").owlCarousel({
+        navigation : true, // Show next and prev buttons
+          slideSpeed : 300,
+          paginationSpeed : 400,
+          singleItem:true,
+          center: true,
+          navigationText: ['<i class="fa fa-chevron-circle-left"></i>', '<i class="fa fa-chevron-circle-right"></i>']
+    });
+
 });
